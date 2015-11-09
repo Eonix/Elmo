fb808997eb9975c62b31f893622eb5aaa08d7e22
Martin Aakjær <maa@blueposition.com>
2015-11-09 17:12:29 +0100

    Finished first version of Details page.

--- Elmo/Elmo/Responses/Views/ErrorDetailView.cs
+++ Elmo/Elmo/Responses/Views/ErrorDetailView.cs
@@ -72,9 +72,116 @@ namespace Elmo.Responses.Views
             await writer.WriteStartElementAsync("pre");
             await writer.WriteAttributeStringAsync("id", "ErrorDetail");
             {
-                // TODO: Output exception stacktrace here.
+                await writer.WriteStringAsync(error.Detail);
             }
             await writer.WriteEndElementAsync();
+
+            await writer.WriteStartElementAsync("p");
+            await writer.WriteAttributeStringAsync("id", "ErrorLogTime");
+            {
+                await writer.WriteStringAsync($"Logged on {error.Time}.");
+            }
+            await writer.WriteEndElementAsync();
+
+            await writer.WriteStartElementAsync("p");
+            {
+                await writer.WriteStringAsync("See also:");
+            }
+            await writer.WriteEndElementAsync();
+
+            await writer.WriteStartElementAsync("ul");
+            {
+                await writer.WriteStartElementAsync("li");
+                {
+                    await writer.WriteStringAsync("Raw/Source data in ");
+
+                    await writer.WriteStartElementAsync("a");
+                    await writer.WriteAttributeStringAsync("href", "json" + OwinContext.Request.Uri.Query);
+                    await writer.WriteAttributeStringAsync("rel", "alternate");
+                    await writer.WriteAttributeStringAsync("type", "application/json");
+                    {
+                        await writer.WriteStringAsync("JSON");
+                    }
+                    await writer.WriteEndElementAsync();
+                }
+                await writer.WriteEndElementAsync();
+            }
+            await writer.WriteEndElementAsync();
+
+            await RenderDictionaryAsync(writer, error.ServerEnvironment, "ServerVariables", "Server Environment");
+            await RenderDictionaryAsync(writer, error.Headers.ToDictionary(pair => pair.Key, pair => string.Join(", ", pair.Value)), "ServerVariables", "Headers");
+            await RenderDictionaryAsync(writer, error.Query.ToDictionary(pair => pair.Key, pair => string.Join(", ", pair.Value)), "ServerVariables", "Query");
+            await RenderDictionaryAsync(writer, error.Cookies, "ServerVariables", "Cookies");
+        }
+
+        private static async Task RenderDictionaryAsync(XmlWriter writer, Dictionary<string, string> dictionary, string id, string title)
+        {
+            if (!dictionary.Any())
+                return;
+
+            await writer.WriteStartElementAsync("div");
+            await writer.WriteAttributeStringAsync("id", id);
+            {
+                await writer.WriteStartElementAsync("p");
+                await writer.WriteAttributeStringAsync("class", "table-caption");
+                {
+                    await writer.WriteStringAsync(title);
+                }
+                await writer.WriteEndElementAsync();
+
+                await writer.WriteStartElementAsync("div");
+                await writer.WriteAttributeStringAsync("class", "scroll-view");
+                {
+                    await writer.WriteStartElementAsync("table");
+                    {
+                        await writer.WriteStartElementAsync("tr");
+                        {
+                            await writer.WriteStartElementAsync("th");
+                            await writer.WriteAttributeStringAsync("class", "name-col");
+                            {
+                                await writer.WriteStringAsync("Name");
+                            }
+                            await writer.WriteEndElementAsync();
+
+                            await writer.WriteStartElementAsync("th");
+                            await writer.WriteAttributeStringAsync("class", "value-col");
+                            {
+                                await writer.WriteStringAsync("Value");
+                            }
+                            await writer.WriteEndElementAsync();
+                        }
+                        await writer.WriteEndElementAsync();
+
+                        var count = 0;
+                        foreach (var line in dictionary)
+                        {
+                            await writer.WriteStartElementAsync("tr");
+                            await writer.WriteAttributeStringAsync("class", count%2 == 0 ? "even-row" : "odd-row");
+                            {
+                                await writer.WriteStartElementAsync("td");
+                                await writer.WriteAttributeStringAsync("class", "key-col");
+                                {
+                                    await writer.WriteStringAsync(line.Key);
+                                }
+                                await writer.WriteEndElementAsync();
+
+                                await writer.WriteStartElementAsync("td");
+                                await writer.WriteAttributeStringAsync("class", "value-col");
+                                {
+                                    await writer.WriteStringAsync(line.Value);
+                                }
+                                await writer.WriteEndElementAsync();
+                            }
+                            await writer.WriteEndElementAsync();
+                        }
+                    }
+                    await writer.WriteEndElementAsync();
+                }
+                await writer.WriteEndElementAsync();
+            }
+            await writer.WriteEndElementAsync();
+
+            await writer.WriteElementStringAsync("br", string.Empty);
         }
 
         protected override async Task LoadContentsAsync()
