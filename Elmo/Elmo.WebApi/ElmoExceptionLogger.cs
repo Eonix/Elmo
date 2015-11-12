2117753dafc45b9c8a3d27f3850c3813fd9ae059
Martin Aakjær <maa@blueposition.com>
2015-11-12 17:05:33 +0100

    Partial implementation of Web API Exception Logging to Elmo

---
+++ Elmo/Elmo.WebApi/ElmoExceptionLogger.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using Elmo.Logging;
+
+namespace Elmo.WebApi
+{
+    public class ElmoExceptionLogger : ExceptionLogger
+    {
+        private readonly IErrorLog errorLog;
+
+        public ElmoExceptionLogger(IErrorLog errorLog)
+        {
+            this.errorLog = errorLog;
+        }
+
+        public override async Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            // TODO: Find out if we instead of logging to an instance should save the exception into Owin Environment and fetch it in the ElmoMiddleware.
+            // Ref: http://stackoverflow.com/questions/31744146/replace-iexceptionhandler-in-web-api-2-2-with-owin-middleware-exception-handler
+            await errorLog.LogAsync(new Error(context.Exception, context.Request.GetOwinContext()));
+            await base.LogAsync(context, cancellationToken);
+        }
+    }
+}
