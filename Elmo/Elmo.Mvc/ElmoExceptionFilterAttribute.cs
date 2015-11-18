b873e995cd20e3507fdd15a08b2f23893979132b
Martin Aakjær <maa@blueposition.com>
2015-11-18 10:00:22 +0100

    Added Elmo MVC support.

---
+++ Elmo/Elmo.Mvc/ElmoExceptionFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace Elmo.Mvc
+{
+    public class ElmoExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            var owinContext = actionExecutedContext.Request.GetOwinContext();
+            if (owinContext == null)
+                throw new InvalidOperationException("Owin Context is not available.");
+
+            owinContext.Set(ElmoConstants.ExceptionKey, actionExecutedContext.Exception);
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
