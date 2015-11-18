97666b71be8ad8491c2e28338c2a1b3de637151a
Martin Aakjær <maa@blueposition.com>
2015-11-18 09:35:22 +0100

    Added check before logging to Owin.

--- Elmo/Elmo.WebApi/ElmoExceptionLogger.cs
+++ Elmo/Elmo.WebApi/ElmoExceptionLogger.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.ExceptionHandling;
@@ -10,6 +11,9 @@ namespace Elmo.WebApi
         public override async Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
         {
             var owinContext = context.Request.GetOwinContext();
+            if (owinContext == null)
+                throw new InvalidOperationException("Owin Context is not available.");
+
             owinContext.Set(ElmoConstants.ExceptionKey, context.Exception);
             await base.LogAsync(context, cancellationToken);
         }
